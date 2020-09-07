2afe8dd91057b2834d2ca5133cf44892bce47fda
Anthony <anthony.x@shopperplus.com>
2020-09-07 17:00:48 +0800

    添加注解

--- app/src/main/java/com/xie/rlrecyclerview/MainActivity.kt
+++ app/src/main/java/com/xie/rlrecyclerview/MainActivity.kt
@@ -44,7 +44,9 @@ class MainActivity : AppCompatActivity() {
         rl_rv.layoutManager = StaggeredGridLayoutManager(2, RecyclerView.VERTICAL)
         adapter = MyAdapter()
         rl_rv.adapter = adapter
+        //打开下拉刷新开关
         rl_rv.refreshEnable = true
+        //设置下拉刷新回调
         rl_rv.onRefreshListener = object : OnRefreshListener {
             override fun onRefresh() {
                 //刷新
@@ -54,12 +56,15 @@ class MainActivity : AppCompatActivity() {
                     listData.addAll(getListData(page, 30))
                     //用替换数据的方式刷新列表
                     adapter.updateList(UpdateList(UpdateType.REFRESH_LIST, listData))
+                    //刷新或加载完成，隐藏刷新和加载UI
                     rl_rv.setRLState(RLRecyclerState.NORMAL)
                     showTestResult = false
                 }, 2000)
             }
         }
-        rl_rv.autoLoadMoreEnable = true
+        //打开自动加载开关
+        //第二个参数为剩下多少个item未展示时触发下一页的加载
+        rl_rv.setAutoLoadMoreEnable(true,2)
         rl_rv.onLoadMoreListener = object : OnLoadMoreListener {
             override fun onLoadMore() {
                 //加载更多
@@ -78,6 +83,7 @@ class MainActivity : AppCompatActivity() {
                         showTestResult = true
                     } else {
                         loadNextPage()
+                        //刷新或加载完成，隐藏刷新和加载UI
                         rl_rv.setRLState(RLRecyclerState.NORMAL)
                     }
                 }, 2000)
--- app/src/main/java/com/xie/rlrecyclerview/MyAdapter.kt
+++ app/src/main/java/com/xie/rlrecyclerview/MyAdapter.kt
@@ -9,6 +9,7 @@ import com.xie.librlrecyclerview.base.RLRecyclerAdapter
  * Created by Anthony on 2020/9/4.
  * Describe:
  */
+//String为列表数据的类型，可根据自己的需要传入不同的类
 class MyAdapter : RLRecyclerAdapter<String>() {
 
     override fun onCreateViewHolderNew(parent: ViewGroup, viewType: Int): BaseRecyclerViewHolder {
@@ -20,6 +21,7 @@ class MyAdapter : RLRecyclerAdapter<String>() {
     }
 
     override fun onBindViewHolderNew(holder: BaseRecyclerViewHolder, position: Int) {
+        //通过getListData()获取列表数据，在这个例子里getListData()返回ArrayList<String>
         (holder.contentView as TextView).text = getListData()[position]
     }
 
